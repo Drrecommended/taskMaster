84ff3d50fceef1eb273856cf71b05d9c36afc1c5
Tyler French <tylerfrench89@gmail.com>
2023-01-04 15:19:09 -0800

    nav functionality with open and closing now working

--- src/UI.js
+++ src/UI.js
@@ -1,6 +1,10 @@
 const interFace = () => {
-    const navBtn = document.getElementById('nav-btn')
-    navBtn.addEventListener('click', () => console.log('hey hey'))
+  //NAV CONTROLS
+  const nav = document.getElementById('nav')
+  const navBtn = document.getElementById('nav-btn')
+  const closeNavBtn = document.getElementById('close-nav-btn')
+  navBtn.addEventListener('click', () => (nav.style.width = '250px'))
+  closeNavBtn.addEventListener('click', () => (nav.style.width = '0px'))
 }
 
 export default interFace
