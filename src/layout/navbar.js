6bf6c6c50e1c59baa2847fe436bee99849a1c3d0
Tyler French <tylerfrench89@gmail.com>
2023-03-09 18:16:08 -0800

    started styling the nav

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -1,22 +1,25 @@
-export default function navbar() {
-  const nav = document.getElementById('nav')
+const nav = document.getElementById('nav')
 
+export default function navbar() {
   const navLinks = document.querySelectorAll('nav a')
   const closeNavBtn = document.getElementById('close-nav_btn')
 
-  const navigate = () => {
-    
+  const navigate = () => {}
+
+  const closeNav = () => {
+    nav.classList.remove('open')
   }
 
-  
+  closeNavBtn.addEventListener('click', closeNav)
 
   navLinks.forEach((link) => {
     link.addEventListener('click', navigate)
   })
 }
 
-function openNav() {
-  console.log('test')
+const openNav = () => {
+  console.log('testing')
+  nav.classList.add('open')
 }
 
 export { openNav }
