e2c6fdb19a7cc93ccc88087a35ab027600d09220
Tyler French <tylerfrench89@gmail.com>
2023-03-23 16:52:57 -0700

    able to toggle form view now

--- src/layout/mainContent.js
+++ src/layout/mainContent.js
@@ -1,3 +1,15 @@
 export default function mainContent() {
+  const sectionTitle = document.getElementById('section__title')
+  const form = document.getElementById('task-form')
+  const openFormBtn = document.getElementById('open-form__btn')
 
+  const toggleFormView = () => {
+    if (form.style.display === 'block') {
+      form.style.display = 'none'
+    } else {
+      form.style.display = 'block'
+    }
+  }
+
+  openFormBtn.addEventListener('click', toggleFormView)
 }
